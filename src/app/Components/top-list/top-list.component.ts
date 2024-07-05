0b28149a9f59f7af2134442e70ee1f34e57c9184
LNAG\holgerh <holgerh@amana.lucanet.com>
2024-07-05 14:36:11 +0200

    2024-07-05 Top-List Drag Order

--- src/app/Components/top-list/top-list.component.ts
+++ src/app/Components/top-list/top-list.component.ts
@@ -1,15 +1,33 @@
+// https://accesto.com/blog/drag-and-drop-angular/
+
 import { Component } from '@angular/core';
 import { BookService } from '../../Services/book.service';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Book } from '../../Classes/Book';
 
 @Component({
   selector: 'app-top-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DragDropModule],
   templateUrl: './top-list.component.html',
   styleUrl: './top-list.component.css',
 })
-
 export class TopListComponent {
-  constructor(public bookservice: BookService) {}
+
+  constructor(public bookservice: BookService) { }
+
+  drop(event: CdkDragDrop<string[]>) {
+    // alert('drop ' + event.previousIndex + ' to ' + event.currentIndex);
+    moveItemInArray(
+      this.bookservice.topBooks,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
--- src/app/Services/book.service.ts
+++ src/app/Services/book.service.ts
@@ -2,38 +2,46 @@ import { Injectable } from '@angular/core';
 import { Book } from '../Classes/Book'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class BookService {
-
   books = [
-    new Book("Foundation", "Asimov, Isaac", "3-453-07988-4", 450, 29.99),
-    new Book("Rendezvous with Rama", "Clark, Arthur C.", "8-808-40483-2", 310, 9.49),
-    new Book("The dying Earth", "Vance, Jack", "1-946-18452-9", 250, 9.88),
-    new Book("Dune", "Herbert, Frank", "3-555-74837-5", 650, 14.99),
-    new Book("War of the Worlds", "Wells, H.G.", "1-277-18988-3", 210, 6.50),
-    new Book("The Demolished Man", "Bester, Alfred", "7-483-28911-9", 195, 6.80),
-    new Book("Ender's Game", "Scott Card, Orson", "6-845-18927-6", 195, 12.50),
-   // new Book("Fahrenheit 451", "Bradbury, Ray", "2-227-48372-4", 225),
-   // new Book("The Hitchhiker's Guide", "Adams, Douglas", "4-828-54324-4", 295),
-   // new Book("Hyperion", "Simmons, Dan", "1-123-99573-3", 295)
+    new Book('Foundation', 'Asimov, Isaac', '3-453-07988-4', 450, 29.99),
+    new Book(
+      'Rendezvous with Rama',
+      'Clark, Arthur C.',
+      '8-808-40483-2',
+      310,
+      9.49
+    ),
+    new Book('The dying Earth', 'Vance, Jack', '1-946-18452-9', 250, 9.88),
+    new Book('Dune', 'Herbert, Frank', '3-555-74837-5', 650, 14.99),
+    new Book('War of the Worlds', 'Wells, H.G.', '1-277-18988-3', 210, 6.5),
+    new Book('The Demolished Man', 'Bester, Alfred', '7-483-28911-9', 195, 6.8),
+    new Book("Ender's Game", 'Scott Card, Orson', '6-845-18927-6', 195, 12.5),
+    // new Book("Fahrenheit 451", "Bradbury, Ray", "2-227-48372-4", 225),
+    // new Book("The Hitchhiker's Guide", "Adams, Douglas", "4-828-54324-4", 295),
+    // new Book("Hyperion", "Simmons, Dan", "1-123-99573-3", 295)
   ];
 
-  filterdBooks = this.books;
-  isFiltered : boolean = false;
 
-  constructor() { }
+  filterdBooks = this.books;
+
+  topBooks = this.books;
+
+  isFiltered: boolean = false;
+
+  constructor() {}
 
   add(book: Book) {
-    this.books.push(book)
+    this.books.push(book);
   }
 
   addOrUpdate(book: Book) {
-    let index = this.books.findIndex(d => d.Id === book.Id);
+    let index = this.books.findIndex((d) => d.Id === book.Id);
     if (index === -1) {
       // alert("This is a new book!");
-      this.books.push(book)
+      this.books.push(book);
     } else {
       // Update at index
       // alert("This is a known book!");
@@ -45,37 +53,33 @@ export class BookService {
   }
 
   delete(book: Book) {
-    let index = this.books.findIndex(d => d.Id === book.Id); //find index in your array
+    let index = this.books.findIndex((d) => d.Id === book.Id); //find index in your array
     if (index != -1) {
       this.books.splice(index, 1);
     }
   }
 
-
   getById(id: string): Book {
-    let b = this.books.filter(i => i.Id === id)[0];
+    let b = this.books.filter((i) => i.Id === id)[0];
     return b;
   }
 
   filter(text: string) {
-    if (text === "") {
+    if (text === '') {
       this.isFiltered = false;
       this.filterdBooks = this.books;
     } else {
       this.filterdBooks = [];
       this.isFiltered = true;
-      this.books.forEach(book => {
-
+      this.books.forEach((book) => {
         let lowerText = text.toLowerCase();
-        
+
         if (book.Title.toLowerCase().includes(lowerText)) {
-          this.filterdBooks.push(book);          
-        }
-        else if (book.Autor.toLowerCase().includes(lowerText)) {
-          this.filterdBooks.push(book);          
-        }
-        else if (book.ISBN.toLowerCase().includes(lowerText)) {
-          this.filterdBooks.push(book);          
+          this.filterdBooks.push(book);
+        } else if (book.Autor.toLowerCase().includes(lowerText)) {
+          this.filterdBooks.push(book);
+        } else if (book.ISBN.toLowerCase().includes(lowerText)) {
+          this.filterdBooks.push(book);
         }
       });
     }
